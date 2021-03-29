17df04186eed5d702ddb1dfd8ddf2a204c897820
Arturo <Arturo.O2000@hotmail.com>
2021-03-29 16:28:50 -0400

    division

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ def main():
     saludar()
     print(sumaNumeros(5, 6))
     print(restaNumeros(6, 5))
+    print(division(2, 0))
+
+
+def division(a, b):
+    if b != 0:
+        return a, b
+    else:
+        return "no se puede dividir entre 0"
 
 
 def sumaNumeros(a, b):
